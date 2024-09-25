1085f70e239897f6ab2aad39f486bc5401a13d96
fathima <fathimaferonzia2004@gmail.com>
2024-09-25 18:54:51 +0530

    git commit

---
+++ frontend/src/actions/productsActions.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import { productsFail, productsRequest, productsSuccess } from '../slices/productsSlice';
+
+export const getProducts = (keyword, price, currentPage) => async (dispatch) => {
+    try{
+        dispatch(productsRequest());
+        let link = `/api/v1/products?page=${currentPage}`;
+
+        if(keyword){
+            link += `&keyword=${keyword}`
+        }
+        if(price){
+            link += `&price[gte]=${price[0]}&price[lte]=${price[1]}`
+        }
+
+        const { data } = await axios.get(link);
+        dispatch(productsSuccess(data))
+
+    }catch(error){
+        //handle error
+        dispatch(productsFail(error.response.data.message))
+    }
+   
+}
